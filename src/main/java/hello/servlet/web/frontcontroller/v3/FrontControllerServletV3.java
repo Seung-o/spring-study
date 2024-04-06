d6865620ba305996773a0c9167c33518bf680c5e
seungo <ilkhso@gmail.com>
2024-04-06 23:52:28 +0900

    feat: Model 추가 - v3

---
+++ src/main/java/hello/servlet/web/frontcontroller/v3/FrontControllerServletV3.java
@@ -0,0 +1,64 @@
+package hello.servlet.web.frontcontroller.v3;
+
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.Serial;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name = "FrontControllerServletV3", urlPatterns = "/front-controller/v3/*")
+public class FrontControllerServletV3 extends HttpServlet {
+
+    @Serial
+    private static final long serialVersionUID = -4849238501611726706L;
+    private final Map<String, ControllerV3> controllerV3Map = new HashMap<>();
+
+
+    public FrontControllerServletV3() {
+        controllerV3Map.put("/front-controller/v3/members/new-form", new MemberFormControllerV3());
+        controllerV3Map.put("/front-controller/v3/members/save", new MemberSaveControllerV3());
+        controllerV3Map.put("/front-controller/v3/members", new MemberListControllerV3());
+    }
+
+    private static MyView viewResolver(ModelView modelView) {
+        MyView myView = new MyView("/WEB-INF/views/" + modelView.getViewName() + ".jsp");
+        return myView;
+    }
+
+    private static Map<String, String> createParamMap(HttpServletRequest request) {
+        Map<String, String> paramMap = new HashMap<>();
+        request.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> paramMap.put(paramName, request.getParameter(paramName)));
+        return paramMap;
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("FrontControllerServletV2.service");
+        String requestURI = request.getRequestURI();
+        ControllerV3 controllerV3 = controllerV3Map.get(requestURI);
+        if (controllerV3 == null) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        // paramMap
+        Map<String, String> paramMap = FrontControllerServletV3.createParamMap(request);
+
+        ModelView modelView = controllerV3.process(paramMap);
+
+        MyView myView = FrontControllerServletV3.viewResolver(modelView);
+
+        myView.render(modelView.getModel(), request, response);
+    }
+}
